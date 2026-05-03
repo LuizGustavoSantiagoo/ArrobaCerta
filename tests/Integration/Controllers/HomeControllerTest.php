0b170978eab3172889baa8d694c9b044f016be5b
LuizGustavoSantiagoo <luizsantiago@alunos.utfpr.edu.br>
2026-05-03 16:10:57 -0300

    :recycle: refactor: update input field classes in HomeControllerTest for consistency and clarity

--- tests/Integration/Controllers/HomeControllerTest.php
+++ tests/Integration/Controllers/HomeControllerTest.php
@@ -6,8 +6,8 @@ class HomeControllerTest extends ControllerTestCase
 {
     public function test_render_home_page(): void
     {
-        $emailInputPattern = '/<input type="text" class="rounded-lg p-2" name="email" id="email">/';
-        $passwordInputPattern = '/<input type="password" class="rounded-lg p-2" name="password" id="password">/';
+        $emailInputPattern = '/<input type="text" class="border rounded-lg p-2" name="email" id="email">/';
+        $passwordInputPattern = '/<input type="password" class="border rounded-lg p-2" name="password" id="password">/';
 
 
         $response = $this->get(
